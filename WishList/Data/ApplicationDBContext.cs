4307ce4a7c85b9aa352cb2da4970539fe21df013
sanjayvk <sanjay.sannova@gmail.com>
2020-07-03 16:45:09 +0530

    Add files via upload

---
+++ WishList/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        
+        public ApplicationDBContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Item> items { get; set; }
+    }
+}
